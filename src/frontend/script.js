c4457523dc17198f03e05fdb772c85307884a30a
Basel <mrbasel555@gmail.com>
2023-02-17 01:44:22 +0300

    add simple keypad

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -1,5 +1,12 @@
 import { Chip8 } from "../chip8.js";
 
+const keys = [
+    '1', '2', '3', '4',
+    'Q', 'W', 'E', 'R',
+    'A', 'S', 'D', 'F',
+    'Z', 'X', 'C', 'V'
+]
+
 const fileInput = document.querySelector('input');
 const onColor = "rgb(255, 255, 255)";
 const offColor = "rgb(0, 0, 0)";
@@ -28,7 +35,7 @@ const gameLoop = (emulator) => {
     if (emulator.drawFlag) {
         drawDisplay(emulator.display);
     }
-    window.requestAnimationFrame(() => gameLoop(emulator));
+    setTimeout(() => gameLoop(emulator), 5)
 }
 
 function readFile(fileInput, callback) {
@@ -43,11 +50,29 @@ function readFile(fileInput, callback) {
     }
 }
 
-
 document.querySelector('button').addEventListener('click', () => {
     readFile(fileInput, (view) => {
         const emulator = new Chip8(view);
 
-        window.requestAnimationFrame(() => gameLoop(emulator));
+        document.addEventListener('keydown', (e) => {
+            emulator.keypad[keys.indexOf(e.key.toUpperCase())] = 1;
+        })
+
+        document.addEventListener('keyup', (e) => {
+            emulator.keypad[keys.indexOf(e.key.toUpperCase())] = 0;
+        })
+
+        const keypad = document.querySelector('#keypad')
+        keypad.childNodes.forEach((button) => {
+            button.addEventListener('click', () => {
+                const key = parseInt(button.textContent, 16);
+                emulator.keypad[key] = 1;
+
+                // Temporary hack :/
+                setTimeout(() => emulator.keypad.fill(0), 200);
+            })
+        })
+
+        gameLoop(emulator)
     })
-})
+});
