c1c9df79bc59097aff194ab26dd4eebc09782de3
Basel <mrbasel555@gmail.com>
2023-03-04 14:39:22 +0300

    refactor Chip8 class

--- src/core/chip8.ts
+++ src/core/chip8.ts
@@ -2,9 +2,9 @@ import { fontset } from "./fontset.js";
 import { getNthBit } from "./utils.js";
 
 export class Chip8 {
-  memory = new Uint8Array(4096);
-  registers = new Uint8Array(16);
-  display = new Uint8Array(64 * 32);
+  private _memory = new Uint8Array(4096);
+  private _registers = new Uint8Array(16);
+  private _display = new Uint8Array(64 * 32);
   pc = 0x200;
   indexReg = 0;
   delayTimer: number;
@@ -15,17 +15,29 @@ export class Chip8 {
 
   constructor(programArray: Uint8Array) {
     fontset.forEach((elem, i) => {
-      this.memory[i] = elem;
+      this._memory[i] = elem;
     });
 
-    for (let i = 0; i < this.memory.length; i++) {
-      this.memory[512 + i] = programArray[i];
+    for (let i = 0; i < this._memory.length; i++) {
+      this._memory[512 + i] = programArray[i];
     }
+  }
+
+  get memory() {
+    return this._memory;
+  }
+
+  get registers() {
+    return this._registers;
+  }
+
+  get display() {
+    return this._display;
   }
 
   emulateCycle() {
     // Fetch opcode
-    const opcode = (this.memory[this.pc] << 8) | this.memory[this.pc + 1];
+    const opcode = (this._memory[this.pc] << 8) | this._memory[this.pc + 1];
     this.pc += 2;
 
     // Decode and execute
@@ -47,7 +59,7 @@ export class Chip8 {
           this.pc = this.stack.pop() as number;
         } else {
           console.log("Clear display");
-          this.display.fill(0);
+          this._display.fill(0);
         }
         break;
       case 0x1000:
@@ -62,11 +74,11 @@ export class Chip8 {
 
       case 0x6000:
         console.log(`Set register V${X} to ${NN}`);
-        this.registers[X] = NN;
+        this._registers[X] = NN;
         break;
       case 0x7000:
         console.log(`Add ${NN} to V${X}`);
-        this.registers[X] += NN;
+        this._registers[X] += NN;
         break;
 
       case 0xa000:
@@ -76,72 +88,72 @@ export class Chip8 {
 
       // Skip instructions
       case 0x3000:
-        if (this.registers[X] === NN) this.pc += 2;
+        if (this._registers[X] === NN) this.pc += 2;
         break;
 
       case 0x4000:
-        if (this.registers[X] !== NN) this.pc += 2;
+        if (this._registers[X] !== NN) this.pc += 2;
         break;
 
       case 0x5000:
-        if (this.registers[X] === this.registers[Y]) this.pc += 2;
+        if (this._registers[X] === this._registers[Y]) this.pc += 2;
         break;
 
       case 0x9000:
-        if (this.registers[X] !== this.registers[Y]) this.pc += 2;
+        if (this._registers[X] !== this._registers[Y]) this.pc += 2;
         break;
 
       // Logical and arithmetic instructions
       case 0x8000:
         switch (N) {
           case 0:
-            this.registers[X] = this.registers[Y];
+            this._registers[X] = this._registers[Y];
             break;
 
           case 1:
-            this.registers[X] |= this.registers[Y];
+            this._registers[X] |= this._registers[Y];
             break;
 
           case 2:
-            this.registers[X] &= this.registers[Y];
+            this._registers[X] &= this._registers[Y];
             break;
 
           case 3:
-            this.registers[X] ^= this.registers[Y];
+            this._registers[X] ^= this._registers[Y];
             break;
 
           case 4:
-            if (this.registers[X] + this.registers[Y] > 255)
-              this.registers[0xf] = 1;
-            else this.registers[0xf] = 0;
+            if (this._registers[X] + this._registers[Y] > 255)
+              this._registers[0xf] = 1;
+            else this._registers[0xf] = 0;
 
-            this.registers[X] += this.registers[Y];
+            this._registers[X] += this._registers[Y];
             break;
 
           case 5:
-            this.registers[0xf] = 1;
-            if (this.registers[Y] > this.registers[X])
-              this.registers[0xf] = 0;
-            this.registers[X] = this.registers[X] - this.registers[Y];
+            this._registers[0xf] = 1;
+            if (this._registers[Y] > this._registers[X])
+              this._registers[0xf] = 0;
+            this._registers[X] = this._registers[X] - this._registers[Y];
             break;
 
           case 7:
-            this.registers[0xf] = 1;
-            if (this.registers[Y] - this.registers[X] < 0)
-              this.registers[0xf] = 0;
-            this.registers[Y] -= this.registers[X];
+            this._registers[0xf] = 1;
+            if (this._registers[Y] - this._registers[X] < 0)
+              this._registers[0xf] = 0;
+            this._registers[Y] -= this._registers[X];
             break;
 
           case 6:
             // this.registers[X] = this.registers[Y];
-            this.registers[0xf] = this.registers[X] & 0x01;
-            this.registers[X] = this.registers[X] >> 1;
+            this._registers[0xf] = this._registers[X] & 0x01;
+            this._registers[X] = this._registers[X] >> 1;
             break;
 
           case 0xe:
             // this.registers[X] = this.registers[Y];
-            this.registers[0xf] = (this.registers[X] & 0x80) >> 7;
-            this.registers[X] = this.registers[X] << 1;
+            this._registers[0xf] = (this._registers[X] & 0x80) >> 7;
+            this._registers[X] = this._registers[X] << 1;
             break;
 
           default:
@@ -151,20 +163,20 @@ export class Chip8 {
         break;
 
       case 0xb000:
-        this.pc = NNN + this.registers[0x0];
+        this.pc = NNN + this._registers[0x0];
         break;
 
       case 0xc000:
-        this.registers[X] = (Math.random() * 256) & NN;
+        this._registers[X] = (Math.random() * 256) & NN;
         break;
 
       case 0xe000:
         switch (NN) {
           case 0x9e:
-            if (this.keypad[this.registers[X]] === 1) this.pc += 2;
+            if (this.keypad[this._registers[X]] === 1) this.pc += 2;
             break;
           case 0xa1:
-            if (this.keypad[this.registers[X]] !== 1) this.pc += 2;
+            if (this.keypad[this._registers[X]] !== 1) this.pc += 2;
             break;
         }
         break;
@@ -172,17 +184,17 @@ export class Chip8 {
       case 0xf000:
         switch (NN) {
           case 0x07:
-            this.registers[X] = this.delayTimer;
+            this._registers[X] = this.delayTimer;
             break;
           case 0x15:
-            this.delayTimer = this.registers[X];
+            this.delayTimer = this._registers[X];
             break;
           case 0x18:
-            this.soundTimer = this.registers[X];
+            this.soundTimer = this._registers[X];
             break;
 
           case 0x1e:
-            this.indexReg += this.registers[X];
+            this.indexReg += this._registers[X];
             break;
 
           case 0x0a:
@@ -190,16 +202,16 @@ export class Chip8 {
             const pressedKey = this.keypad[pressedKeyIndex];
 
             if (!pressedKey) this.pc -= 2;
-            else this.registers[X] = pressedKeyIndex;
+            else this._registers[X] = pressedKeyIndex;
 
             break;
 
           case 0x29:
-            this.indexReg = this.registers[X] * 5;
+            this.indexReg = this._registers[X] * 5;
             break;
 
           case 0x33:
-            let targetNumber = this.registers[X];
+            let targetNumber = this._registers[X];
             const bcdNumber = Array(3).fill(0);
 
             let i = 2;
@@ -208,26 +220,26 @@ export class Chip8 {
               targetNumber = Math.floor(targetNumber / 10)
               i--;
             }
-            this.memory[this.indexReg] = bcdNumber[0];
-            this.memory[this.indexReg + 1] = bcdNumber[1];
-            this.memory[this.indexReg + 2] = bcdNumber[2];
+            this._memory[this.indexReg] = bcdNumber[0];
+            this._memory[this.indexReg + 1] = bcdNumber[1];
+            this._memory[this.indexReg + 2] = bcdNumber[2];
 
             break;
 
           case 0x55:
-            if (X === 0) this.memory[this.indexReg] = this.registers[X];
+            if (X === 0) this._memory[this.indexReg] = this._registers[X];
             else {
               for (let i = 0; i <= X; i++) {
-                this.memory[this.indexReg + i] = this.registers[i];
+                this._memory[this.indexReg + i] = this._registers[i];
               }
             }
             break;
 
           case 0x65:
-            if (X === 0) this.registers[X] = this.memory[this.indexReg];
+            if (X === 0) this._registers[X] = this._memory[this.indexReg];
             else {
               for (let i = 0; i <= X; i++) {
-                this.registers[i] = this.memory[this.indexReg + i];
+                this._registers[i] = this._memory[this.indexReg + i];
               }
             }
             break;
@@ -236,21 +248,21 @@ export class Chip8 {
         break;
 
       case 0xd000:
-        let [xCord, yCord] = [this.registers[X], this.registers[Y]];
+        let [xCord, yCord] = [this._registers[X], this._registers[Y]];
         if (xCord > 64) xCord = xCord - 64;
         if (yCord > 32) yCord = yCord - 32;
         console.log(`Draw at (${xCord}, ${yCord})`);
 
-        this.registers[0xf] = 0;
+        this._registers[0xf] = 0;
         for (let i = 0; i < N; i++) {
           let cy = yCord + i;
           for (let j = 0; j < 8; j++) {
             let cx = xCord + j;
-            const pixel = this.memory[this.indexReg + i];
+            const pixel = this._memory[this.indexReg + i];
             if (getNthBit(pixel, 7 - j) !== 0) {
-              if (this.display[64 * cy + cx] === 1) this.registers[0xf] = 1;
+              if (this._display[64 * cy + cx] === 1) this._registers[0xf] = 1;
 
-              this.display[64 * cy + cx] ^= 1;
+              this._display[64 * cy + cx] ^= 1;
             }
           }
         }
